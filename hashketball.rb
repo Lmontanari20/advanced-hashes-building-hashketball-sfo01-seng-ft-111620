ddea6472d5fb50fcb9396eaeef1278614c5d0570
Lucas Montanari <lmonty2011@gmail.com>
2020-11-03 23:45:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,24 @@
 # Write your code here!
+def game_hash()
+  
+  game = {
+    home: {
+      team_name: "Brooklyn Nets", 
+      colors: ["Black", "White"], 
+      players: [  {
+        player_name: "",
+        number: ,
+        shoe: ,
+        points: , 
+        rebounds: , 
+        assists: , 
+        steals: , 
+        blocks: , 
+        slam_dunks: 
+        }
+      ],
+    },
+    away: {}
+  }
+  
+end
